c63fb1ab3f8e75e831cb9cd8a7f9319e6197c628
Alex Bonilla <alexbonilla@nellys-air.mynetworksettings.com>
2024-10-24 13:34:27 -0400

    Database updated to contain isbn like formatted ids.

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,56 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+let books =
+{
+      "978-1-23456-789-0": {
+            "author": "Chinua Achebe",
+            "title": "Things Fall Apart",
+            "reviews": {}
+      },
+      "978-1-23456-789-1": {
+            "author": "Hans Christian Andersen",
+            "title": "Fairy tales",
+            "reviews": {}
+      },
+      "978-1-23456-789-2": {
+            "author": "Dante Alighieri",
+            "title": "The Divine Comedy",
+            "reviews": {}
+      },
+      "978-1-23456-789-3": {
+            "author": "Unknown",
+            "title": "The Epic Of Gilgamesh",
+            "reviews": {}
+      },
+      "978-1-23456-789-4": {
+            "author": "Unknown",
+            "title": "The Book Of Job",
+            "reviews": {}
+      },
+      "978-1-23456-789-5": {
+            "author": "Unknown",
+            "title": "One Thousand and One Nights",
+            "reviews": {}
+      },
+      "978-1-23456-789-6": {
+            "author": "Unknown",
+            "title": "Njál's Saga",
+            "reviews": {}
+      },
+      "978-1-23456-789-7": {
+            "author": "Jane Austen",
+            "title": "Pride and Prejudice",
+            "reviews": {}
+      },
+      "978-1-23456-789-8": {
+            "author": "Honoré de Balzac",
+            "title": "Le Père Goriot",
+            "reviews": {}
+      },
+      "978-1-23456-789-9": {
+            "author": "Samuel Beckett",
+            "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
+            "reviews": {}
+      }
+};
 
-module.exports=books;
+
+module.exports = books;
